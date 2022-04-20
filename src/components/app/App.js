a20a156aef2e0a17b0754b6c9caf66615f78ff84
aRyabintcev <aRyabintcev@mail.ru>
2022-04-21 00:52:15 +0300

    изменил обработчик ошибок

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,12 @@
 /* eslint-disable import/order */
 import React, { Component } from 'react';
-
+import { Alert } from 'antd';
 import CardList from '../cardList/CardList';
 import MovieDbService from '../../services/MovieDBService';
+import 'antd/dist/antd.css';
 import './App.css';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.getMovies();
-  }
-
   state = {
     movies: [],
     searchQuery: 'return',
@@ -19,6 +15,9 @@ export default class App extends Component {
     error: false,
     errorMsg: '',
   };
+  componentDidMount() {
+    this.getMovies();
+  }
   onError = (err) => {
     this.setState({ error: true, loading: false, errorMsg: err.message });
   };
@@ -36,7 +35,11 @@ export default class App extends Component {
     const { movies, loading, error, errorMsg } = this.state;
     return (
       <div className="container">
-        <CardList data={movies} loading={loading} error={error} errorMsg={errorMsg} />
+        {error ? (
+          <Alert message="Error" description={errorMsg} type="error" showIcon />
+        ) : (
+          <CardList data={movies} loading={loading} error={error} errorMsg={errorMsg} />
+        )}
       </div>
     );
   }
--- src/components/cardList/CardList.js
+++ src/components/cardList/CardList.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Spin, Alert } from 'antd';
+import { Spin } from 'antd';
 
 import 'antd/dist/antd.css';
 import MovieCard from '../movieCard/MovieCard';
 import './CardList.css';
 
-function CardList({ data, loading, error, errorMsg }) {
+function CardList({ data, loading }) {
   const listItems = data.map((movie) => {
     const { id } = movie;
     return <MovieCard key={id} data={movie} />;
   });
-  if (error) {
-    return <Alert message="Error" description={errorMsg} type="error" showIcon />;
-  }
+
   return <div className="card-list">{loading ? <Spin size="large" /> : listItems}</div>;
 }
 export default CardList;
